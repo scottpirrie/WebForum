5e451ccb82b22bb0ea6374b33a4b0b743ee95b62
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-11-18 16:59:23 +0000

    bug in letting username be used with different capitalisation

--- register.php
+++ register.php
@@ -9,8 +9,9 @@
     ?>
 </head>
 <body>
-
-<h1 class="page-header">Register</h1>
+<div class="page-header col-md-offset-1">
+    <h1>Register</h1>
+</div>
 
 <?php
 
@@ -21,11 +22,11 @@ if (isset($_POST["create"])) {
     $createPass = password_hash($_POST["newPassword"], PASSWORD_BCRYPT);
     $accountCreationSuccess = true;
 
-    $sql = "SELECT `username` FROM `Login`";
+    $sql = "SELECT username FROM `Login`";
     $res = $conn->query($sql);
     if ($res->num_rows > 0) {
         while ($row = $res->fetch_assoc()) {
-            if ($serverPass = $row["username"] == $createUser) {
+            if (strtoupper($row["username"]) == strtoupper($createUser)) {
                 $usernameTaken = true;
                 $accountCreationSuccess = false;
                 break;
@@ -37,6 +38,7 @@ if (isset($_POST["create"])) {
         $sql = "INSERT INTO `Login` (`username`, `password`, `type`) VALUES ('$createUser', '$createPass', '2')";
         $conn->query($sql);
 
+        /*todo change privilege level*/
         $_SESSION['user'] = $createUser;
         $_SESSION['type'] = 2;
         ?>
@@ -53,34 +55,47 @@ if ($usernameTaken) { ?>
 }
 
 
-if ($accountCreationSuccess) {
-    echo "account made, show msg, and auto log them in";
-
+if ($accountCreationSuccess) { ?>
+    <div class="container col-md-8 col-md-offset-2">
+        <div class="panel panel-default">
+            <div class="panel-heading">Successfully registered an account, redirect failed.</div>
+            <div class="panel-body">Please navigate using the menu bar. <br>
+                To make a new account, log out.
+            </div>
+        </div>
+    </div>
+    <?php
 } else {
-    if ($_SESSION['type'] > 0) {
-        echo "log off ples";
+    if ($_SESSION['type'] > 0) { ?>
+        <div class="container col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Can not register an account.</div>
+                <div class="panel-body">You are currently logged in. <br>
+                    To make a new account, log out.
+                </div>
+            </div>
+        </div>
+
+        <?php
     } else { ?>
         <form method="POST" action="register.php" class="form">
 
-
-            <div class="container center-block">
-
-
+            <div class="container col-md-4 col-md-offset-4">
                 <div class="form-group row">
 
-                        <label class="col-sm-2 col-form-label" for="userToBe"> Username </label>
-                        <input class="form-control" id="userToBe" type=text name="newUsername"
-                               minlength="4"
-                               maxlength="15"
-                               required>
+                    <label class="col-sm-2 col-form-label" for="userToBe"> Username </label>
+                    <input class="form-control" id="userToBe" type=text name="newUsername"
+                           minlength="4"
+                           maxlength="15"
+                           required>
 
                 </div>
 
                 <div class="form-group row">
 
-                        <label class="col-sm-2 col-form-label" for="passToBe">Password</label>
-                        <input class="form-control" id="passToBe" type=password name="newPassword" minlength="4"
-                               maxlength="15" required>
+                    <label class="col-sm-2 col-form-label" for="passToBe">Password</label>
+                    <input class="form-control" id="passToBe" type=password name="newPassword" minlength="4"
+                           maxlength="15" required>
 
                 </div>
 
