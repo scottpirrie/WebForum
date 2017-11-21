e5bd905fbbf6865fc63ed40cb3589e607fc4ccdf
xbb15134 <kuen.chan.2015@uni.strath.ac.uk>
2017-11-21 23:22:47 +0000

    fixed thing i broke the initial comment

--- newthread.php
+++ newthread.php
@@ -25,36 +25,36 @@ if ($_SESSION["type"] > 0) {
         <div class="panel panel-default">
             <div class="panel-heading">Create a new thread topic.</div>
             <div class="panel-body">
-                <form id="create-thread" class="form" method="post" action="home.php">
+                <form id = "create-thread" class="form" method="post" action="home.php">
 
                     <div class="form-group  col-md-4 ">
-                        <label for="threadName">Thread name:</label>
-                        <input class="form-control" type="text" id="threadName" name="threadName" required
-                               minlength="2" maxlength="30">
+                        <label class="col-form-label" for="threadName">Thread name:</label>
+                        <input  class="form-control" type="text" id="threadName" name="threadName" required minlength="2" maxlength="30">
                     </div>
 
                     <div class="form-group  col-md-12 ">
-                        <label for="post">Post:</label>
-                        <textarea class="form-control" rows="15"  name="post"
-                                  form="create-thread"></textarea><br>
+                        <label class="col-form-label" for="post">Post:</label>
+                        <textarea class="form-control" rows="20" name="post" form="create-thread"></textarea><br>
+                    </div>
 
-            </div>
-                    <div class="form-group  col-md-12 ">
-                        <input type="submit" name="create" value="Create!"></div>
+                    <div class="form-group  col-md-2 ">
+                        <input class="form-control"  type="submit" name="create" value="Create!">
+                    </div>
+
                 </form>
             </div>
         </div>
     </div>
 
     <?php
-} elseif ($_SESSION["type"] == 0) { ?>
+} elseif($_SESSION["type"] == 0) { ?>
     <div class="container col-md-8 col-md-offset-2">
         <div class="panel panel-warning">
             <div class="panel-heading">Not logged in!</div>
             <div class="panel-body">You need to be logged in to create a thread!</div>
         </div>
     </div> <?php
-} elseif ($_SESSION["type"] == -1) { ?>
+} elseif ($_SESSION["type"] == -1){?>
     <div class="container col-md-8 col-md-offset-2">
         <div class="panel panel-warning">
             <div class="panel-heading">Banned!</div>
